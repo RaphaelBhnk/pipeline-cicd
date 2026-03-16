7378d549fb1ebfc33de53657a1dd6d96c72a23d6
RaphaelBhnk <raphael.bouhnik@edu.ece.fr>
2026-03-16 10:59:32 +0100

    tests

---
+++ tests/sum.test.js
@@ -0,0 +1,13 @@
+const { sum } = require("../src/sum");
+
+test("1 + 2 equals to 3", () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test("3 + 2 equals to 3", () => {
+  expect(sum(3, 2)).toBe(5);
+});
+
+test("3 - 1 equals to 3", () => {
+  expect(sum(3, -1)).toBe(2);
+});
